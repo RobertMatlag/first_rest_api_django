b02e7b388dda49a1d4e37b69b1ea001f48f3c69c
RobertMatlag <robert.matlag@gmail.com>
2021-01-21 14:53:37 +0100

    Display queries on server logs

--- tw_rest_api/tw_rest_api/settings.py
+++ tw_rest_api/tw_rest_api/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'spotify.logging_middleware.LoggingMiddleware'
 ]
 
 ROOT_URLCONF = 'tw_rest_api.urls'
